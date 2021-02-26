b73bc5a18b6273f4fdf077a34cf64493eab7cf32
Davi Filho <davifilho.br@gmail.com>
2021-02-26 04:13:18 -0300

    Arquivos do curso de JS

---
+++ Aula 10/ex008.js
@@ -0,0 +1,6 @@
+var val = 75
+console.log(`A velocidade do seu carro é ${val} Km/h`)
+if(val > 60) {
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO`)
+}
+console.log(`Diriga sempre com segurança`)
---
+++ Aula 12ex/ex015/script.js
@@ -0,0 +1,5 @@
+function idade (){
+    var idade = window.document.getElementById('idade')
+    var MF = window.document.getElementById('MF')
+    var foto = window.document.getElementById('foto')
+}
